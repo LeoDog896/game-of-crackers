1ef4a8eb58ab678566ac5c9a7ee6a3f3961baa0e
Tristan F <leodog896@gmail.com>
2023-07-10 11:18:37 -0400

    feat: grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Canvas, Layer, type Render } from 'svelte-canvas';
+
+    let width = 640;
+    let height = 640;
+
+    interface Vector {
+        x: number;
+        y: number;
+    }
+
+    let cellDimensions: Vector = { x: 10, y: 10 };
+
+	let render: Render;
+	$: render = ({ context, width, height }) => {
+        // draw a grid
+        context.strokeStyle = '#ccc';
+        context.lineWidth = 1;
+        for (let x = 0; x < cellDimensions.x; x++) {
+            context.beginPath();
+            context.moveTo(x * width / cellDimensions.x, 0);
+            context.lineTo(x * width / cellDimensions.x, height);
+            context.stroke();
+        }
+
+        for (let y = 0; y < cellDimensions.y; y++) {
+            context.beginPath();
+            context.moveTo(0, y * height / cellDimensions.y);
+            context.lineTo(width, y * height / cellDimensions.y);
+            context.stroke();
+        }
+	};
+</script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<Canvas {width} {height}>
+	<Layer {render} />
+</Canvas>
