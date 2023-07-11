e9d7e20e44adacff93d96a5390a9a2da60a9253b
Tristan F <leodog896@gmail.com>
2023-07-10 21:32:03 -0400

    feat: collisions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,68 @@
     let isRotating = false;
     let currentRotation = 0;
 
+	type Cracker = [position: Vector, rotation: number]
+
+	function corners(cracker: Cracker, size: Vector): Vector[] {
+		const [position, rotation] = cracker;
+		const { x, y } = position;
+		const { x: w, y: h } = size;
+
+		const cos = Math.cos(rotation);
+		const sin = Math.sin(rotation);
+
+		const corners: Vector[] = [
+			{ x: -w / 2, y: -h / 2 },
+			{ x: w / 2, y: -h / 2 },
+			{ x: w / 2, y: h / 2 },
+			{ x: -w / 2, y: h / 2 }
+		]
+
+		const rotatedCorners = corners.map(({ x, y }) => ({
+			x: x * cos - y * sin,
+			y: x * sin + y * cos
+		}));
+
+		return rotatedCorners.map(({ x, y }) => ({
+			x: x + position.x,
+			y: y + position.y
+		}));
+	}
+	
+	function isColliding(size: Vector, crackerA: Cracker, crackerB: Cracker): boolean {
+		// check if 2 crackers are colliding independent of the mouse
+		for (const cracker of crackers) {
+			const points = corners(cracker, size);
+			for (let i1 = 0; i1 < points.length; i1++) {
+				const i2 = (i1 + 1) % points.length;
+				const p1 = points[i1];
+				const p2 = points[i2];
+
+				const normal = { x: p2.y - p1.y, y: p1.x - p2.x };
+
+				let minA = null;
+				let maxA = null;
+				for (const p of corners(crackerA, size)) {
+					const projected = normal.x * p.x + normal.y * p.y;
+					if (minA == null || projected < minA) minA = projected;
+					if (maxA == null || projected > maxA) maxA = projected;
+				}
+
+				let minB = null;
+				let maxB = null;
+				for (const p of corners(crackerB, size)) {
+					const projected = normal.x * p.x + normal.y * p.y;
+					if (minB == null || projected < minB) minB = projected;
+					if (maxB == null || projected > maxB) maxB = projected;
+				}
+
+				if ((maxA && maxB && minA && minB) && (maxA < minB || maxB < minA)) return false;
+			}
+		}
+
+		return true;
+	}
+
 	interface Vector {
 		x: number;
 		y: number;
@@ -67,7 +129,20 @@
 		// draw a rectangle at the cursor of unit size 1
         context.save();
 		context.beginPath();
-		context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+		let collidingCrackers = [];
+
+		for (const cracker of crackers) {
+			if (isColliding(trueSize, cracker, [{ x: cursorX, y: cursorY }, currentRotation])) {
+				collidingCrackers.push(cracker);
+			}
+		}
+
+		if (collidingCrackers.length > 0) {
+			context.fillStyle = "rgba(255, 0, 0, 0.5)"
+		} else {
+			context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+		}
+
         context.translate(cursorX, cursorY);
         context.rotate(currentRotation);
         context.rect(-trueSize.x / 2, -trueSize.y / 2, trueSize.x, trueSize.y);
